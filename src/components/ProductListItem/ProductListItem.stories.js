36a23889f55cc38acf0481dad93e3aedc9435816
Ronald Arias <ariasron@hotmail.com>
2021-11-26 08:22:36 -0500

    Installs Knobs addon to interact with the components

--- src/components/ProductListItem/ProductListItem.stories.js
+++ src/components/ProductListItem/ProductListItem.stories.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import ProductListItem from './ProductListItem'
 import { action } from '@storybook/addon-actions'
+import { text, withKnobs } from '@storybook/addon-knobs'
 
 // original way to crate stories in Storybook
 // import { storiesOf } from '@storybook/react'
 // storiesOf('ProductListItem', module).add('standard', () => <ProductListItem />)
 
 // new way to create stories in Storybook
-export default { title: 'ProductListItem' }
+export default { 
+  title: 'ProductListItem',
+  decorators: [withKnobs] 
+}
+
 export const standard = () => (
   <ProductListItem 
-    name="Standard Coffe"
-    price="2.50"
-    onAddToCart={action('Added to cart')}
-    imageUrl="https://source.unsplash.com/tNALoIZhqVM/200x100/"
+    name={ text("Name", "Standard Coffe") }
+    price={ text("price", "2.50") }
+    onAddToCart={action('Add to cart clicked')}
+    imageUrl={ text("imageUrl", "https://source.unsplash.com/tNALoIZhqVM/200x100/") }
   />
 )
